edcf77ca1f8a6105946b41eceed7f764f4c02557
apsampaio <andre03.sampaio@gmail.com>
2021-09-14 22:09:29 -0300

    add hashed password to user module

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,10 @@ import { UserController } from './user.controller';
 
 import { CreateUniqueID } from './services/CreateUniqueID.service';
 import { PrismaClient } from '@prisma/client';
+import { HashPassword } from './services/HashPassword.service';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService, CreateUniqueID, PrismaClient],
+  providers: [UserService, CreateUniqueID, PrismaClient, HashPassword],
 })
 export class UserModule {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -4,24 +4,30 @@ import { UpdateUserDto } from './dto/update-user.dto';
 
 import { PrismaClient, User } from '@prisma/client';
 import { CreateUniqueID } from './services/CreateUniqueID.service';
+import { HashPassword } from './services/HashPassword.service';
 
 @Injectable()
 export class UserService {
   constructor(
     private prisma: PrismaClient,
     private createUniqueID: CreateUniqueID,
+    private hashPassword: HashPassword,
   ) {}
 
-  async create(createUserDto: CreateUserDto): Promise<User> {
+  async create({ email, name, password, admin }: CreateUserDto): Promise<User> {
     const { uuid } = this.createUniqueID.execute();
-    const user = await this.prisma.user.create({
+
+    const { hashed_password } = await this.hashPassword.execute({ password });
+
+    return await this.prisma.user.create({
       data: {
         id: uuid,
-        ...createUserDto,
+        password: hashed_password,
+        email,
+        name,
+        admin,
       },
     });
-
-    return user;
   }
 
   findAll() {
